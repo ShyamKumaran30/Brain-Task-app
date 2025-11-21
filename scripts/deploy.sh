b68d6cb8dbed73aa50cab08886355fa7f29b86ad
ShyamKumaran30 <146002532+ShyamKumaran30@users.noreply.github.com>
2025-11-21 20:23:13 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+set -e
+
+# Configure kubeconfig for EKS
 aws eks --region us-east-1 update-kubeconfig --name brain-tasks-cluster
+
+# Apply Kubernetes manifests
 kubectl apply -f k8s/deployment.yaml
+
+# Wait until deployment is successful
+kubectl rollout status deployment/brain-tasks-app
